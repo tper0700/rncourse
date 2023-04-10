560b16587db4d237e5e8375d51656b2deb5352bf
Tom Perez <60866122+tper0700@users.noreply.github.com>
2023-04-09 21:57:02 -0700

    Actually swap the faces!

--- homework/hw2/FunCamera/src/components/Swapper.tsx
+++ homework/hw2/FunCamera/src/components/Swapper.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useRef } from 'react';
 import {Asset} from 'react-native-image-picker';
-import RNPhotoManipulator from 'react-native-photo-manipulator';
+import { PhotoManipulator, Point } from 'react-native-photo-manipulator';
 import { Rect } from "./Face";
 
 import {
@@ -39,11 +39,17 @@ const Swapper = function(props: {
     let pathB = props.imgB.uri ? props.imgB.uri : "";
 
     try {
-      let cropA = await RNPhotoManipulator.crop(pathA, props.rectA);
-      let cropB = await RNPhotoManipulator.crop(pathB, props.rectB);
+      let sizeA = {width: props.rectB.width, height: props.rectB.height};
+      let sizeB = {width: props.rectA.width, height: props.rectA.height};
+      let cropA = await PhotoManipulator.crop(pathA, props.rectA, sizeA);
+      let cropB = await PhotoManipulator.crop(pathB, props.rectB, sizeB);
+      let posA : Point = { x: props.rectA.x, y: props.rectA.y };
+      let posB : Point = { x: props.rectB.x, y: props.rectB.y };
+      let swapA = await PhotoManipulator.overlayImage(pathA, cropB, posA)
+      let swapB = await PhotoManipulator.overlayImage(pathB, cropA, posB)
 
-      let asA : Asset = {uri: cropA};
-      let asB : Asset = {uri: cropB};
+      let asA : Asset = {uri: swapA};
+      let asB : Asset = {uri: swapB};
       props.setA(asA);
       props.setB(asB);
     } catch (e) {
